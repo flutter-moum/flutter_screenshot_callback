a7c80d1afdf165767ce12621137c65396af63e83
beygee <doug0476@naver.com>
2019-09-30 15:20:06 +0900

    Dart callback invokeMethod

--- lib/screenshot_callback.dart
+++ lib/screenshot_callback.dart
@@ -1,13 +1,34 @@
 import 'dart:async';
 
+import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
+import 'package:meta/meta.dart';
 
 class ScreenshotCallback {
   static const MethodChannel _channel =
       const MethodChannel('screenshot_callback');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  List<VoidCallback> onCallbacks;
+
+  Future<void> addListener(VoidCallback callback) async {
+    if (callback == null) throw ("No Callback!");
+
+    onCallbacks.add(callback);
+
+    await _channel.invokeMethod("initialize");
+  }
+
+  void _handleMethod(MethodCall call) {
+    switch (call.method) {
+      case 'onCallback':
+        for (var callback in onCallbacks) callback();
+        break;
+      default:
+        throw ('method not defined');
+    }
+  }
+
+  Future<void> dispose() async {
+    await _channel.invokeMethod("dispose");
   }
 }
