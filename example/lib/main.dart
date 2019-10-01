ac560030dd5190bd01eaa9517b1488d531955a52
beygee <doug0476@naver.com>
2019-10-01 14:44:56 +0900

    [Add] Example main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,32 +12,19 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+  ScreenshotCallback screenshotCallback = ScreenshotCallback();
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
+
+    screenshotCallback.addListener(() {});
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await ScreenshotCallback.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
+  @override
+  void dispose() {
+    screenshotCallback.dispose();
+    super.dispose();
   }
 
   @override
@@ -48,7 +35,7 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: Text('Running'),
         ),
       ),
     );
