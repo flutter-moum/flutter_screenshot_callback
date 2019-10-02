de63a52e7966a897f71257879542c6db2f69570a
beygee <doug0476@naver.com>
2019-10-02 19:39:15 +0900

    invokemethod to flutter

--- ios/Classes/SwiftScreenshotCallbackPlugin.swift
+++ ios/Classes/SwiftScreenshotCallbackPlugin.swift
@@ -2,31 +2,31 @@ import Flutter
 import UIKit
 
 public class SwiftScreenshotCallbackPlugin: NSObject, FlutterPlugin {
-  var channel: FlutterMethodChannel? = nil
+  static var channel: FlutterMethodChannel?
 
   public static func register(with registrar: FlutterPluginRegistrar) {
-    channel = FlutterMethodChannel(name: "screenshot_callback", binaryMessenger: registrar.messenger())
+    channel  = FlutterMethodChannel(name: "screenshot_callback", binaryMessenger: registrar.messenger())
+
     let instance = SwiftScreenshotCallbackPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
+    if let channel = channel {
+      registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+    
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     if(call.method == "initialize"){
-      print("iOS screenshotcallback initialized")
-      result("initialize")
       NotificationCenter.default.addObserver(
           forName: NSNotification.Name.UIApplicationUserDidTakeScreenshot,
           object: nil,
           queue: .main) { notification in
-          print("iOS 스크린샷 콜백함수 호출")
-          result("screen shot called")
-
-          if let channel = channel {
-            channel.invokeMethod("onCallback", arguments: args, result: {(r:Any?) -> () in
-
-                      })
+          if let channel = SwiftScreenshotCallbackPlugin.channel {
+            channel.invokeMethod("onCallback", arguments: nil)
           }
+
+          result("screen shot called")
       }
+      result("initialize")
     }else if(call.method == "dispose"){
       result("dispose")
     }else{
