e702a2571cc26f3a4ef503bf77a109ed6e067028
Gwangyu Kim <kwangyu326@gmail.com>
2019-10-03 17:52:32 +0900

    change path name (absolutePath -> SCREENSHOT_PATH)

--- android/src/main/java/com/flutter/moum/screenshot_callback/ScreenshotCallbackPlugin.java
+++ android/src/main/java/com/flutter/moum/screenshot_callback/ScreenshotCallbackPlugin.java
@@ -9,15 +9,14 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
 import android.os.Environment;
 import android.os.FileObserver;
 import java.io.File;
-import android.util.Log;
+
 import android.os.Handler;
 import android.os.Looper;
 
 public class ScreenshotCallbackPlugin implements MethodCallHandler {
-  private static final String absolutePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM) + File.separator + "Screenshots" + File.separator;
+  private static final String SCREENSHOT_PATH = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM) + File.separator + "Screenshots" + File.separator;
   private Handler handler;
   private FileObserver fileObserver;
-
   private static MethodChannel channel;
 
   public static void registerWith(Registrar registrar) {
@@ -29,12 +28,10 @@ public class ScreenshotCallbackPlugin implements MethodCallHandler {
   public void onMethodCall(MethodCall call, Result result) {
     if (call.method.equals("initialize")) {
       handler = new Handler(Looper.getMainLooper());
-      fileObserver = new FileObserver(absolutePath, FileObserver.CREATE){
+      fileObserver = new FileObserver(SCREENSHOT_PATH, FileObserver.CREATE){
         @Override
         public void onEvent(int event, String path) {
             if (event == FileObserver.CREATE) {
-
-              // use Handler to execute invokeMethod in UIThread
               handler.post(new Runnable() {
                 @Override
                 public void run() {
@@ -46,7 +43,6 @@ public class ScreenshotCallbackPlugin implements MethodCallHandler {
       };
       fileObserver.startWatching();
       result.success("initialize");
-
     } else if (call.method.equals("dispose")) {
       fileObserver.stopWatching();
       result.success("dispose");
