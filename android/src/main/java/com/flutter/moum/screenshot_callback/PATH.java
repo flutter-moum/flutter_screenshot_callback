89c23d896b2efaa48aaa84045674adec7af9ed9d
Gwangyu Kim <kwangyu326@gmail.com>
2019-10-09 16:43:02 +0900

    create FileObservers as many as paths. (with no test :-( )

---
+++ android/src/main/java/com/flutter/moum/screenshot_callback/PATH.java
@@ -0,0 +1,21 @@
+package com.flutter.moum.screenshot_callback;
+
+import android.os.Environment;
+
+import java.io.File;
+
+public enum PATH {
+    DCIM(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM) + File.separator + "Screenshots" + File.separator),
+    PICTURES(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES ) + File.separator + "Screenshots" + File.separator);
+
+    final private String path;
+
+    public String getPath() {
+        return path;
+    }
+
+    private PATH(String path) {
+        this. path = path;
+
+    }
+}
--- android/src/main/java/com/flutter/moum/screenshot_callback/ScreenshotCallbackPlugin.java
+++ android/src/main/java/com/flutter/moum/screenshot_callback/ScreenshotCallbackPlugin.java
@@ -6,21 +6,21 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-import android.os.Environment;
 import android.os.FileObserver;
-import java.io.File;
 
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 
 public class ScreenshotCallbackPlugin implements MethodCallHandler {
-  private static final String SCREENSHOT_PATH = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM) + File.separator + "Screenshots" + File.separator;
   private static MethodChannel channel;
 
   private Handler handler;
   private FileObserver fileObserver;
+  private String TAG = "tag";
 
   public static void registerWith(Registrar registrar) {
+    Log.d("flutter", "onMethodCall1");
     channel = new MethodChannel(registrar.messenger(), "screenshot_callback");
     channel.setMethodCallHandler(new ScreenshotCallbackPlugin());
   }
@@ -29,9 +29,12 @@ public class ScreenshotCallbackPlugin implements MethodCallHandler {
   public void onMethodCall(MethodCall call, Result result) {
     if (call.method.equals("initialize")) {
       handler = new Handler(Looper.getMainLooper());
-      fileObserver = new FileObserver(SCREENSHOT_PATH, FileObserver.CREATE){
-        @Override
-        public void onEvent(int event, String path) {
+
+      for(PATH path : PATH.values()){
+        fileObserver = new FileObserver(path.getPath(), FileObserver.CREATE) {
+          @Override
+          public void onEvent(int event, String path) {
+            Log.d(TAG,"onEvent");
             if (event == FileObserver.CREATE) {
               handler.post(new Runnable() {
                 @Override
@@ -40,9 +43,10 @@ public class ScreenshotCallbackPlugin implements MethodCallHandler {
                 }
               });
             }
-        }
-      };
-      fileObserver.startWatching();
+          }
+        };
+        fileObserver.startWatching();
+      }
       result.success("initialize");
     } else if (call.method.equals("dispose")) {
       fileObserver.stopWatching();
